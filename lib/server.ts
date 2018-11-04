a8c9d4f3939e5c5bb9d8aea2e1f79daab5959cc1
Yusuf Can Celik <yusufcancelik@hotmail.com>
2018-11-04 15:24:12 +0300

    fc

--- lib/server.ts
+++ lib/server.ts
@@ -1,9 +1,8 @@
 import app from './app';
 import * as http from 'http';
 import * as fs from 'fs';
-const PORT = 80;
 
-
+const PORT = parseInt(process.env.PORT, 10) || 5000;
 
 http.createServer(app).listen(PORT, () => {
     console.log('Express server listening on port ' + PORT);
