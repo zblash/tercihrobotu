649692100b02acc9fbd8f155f9c0a81482c9b497
Yusuf Can Celik <yusufcancelik@hotmail.com>
2018-11-13 01:32:27 +0300

    Updated

--- dist/app.js
+++ dist/app.js
@@ -18,7 +18,7 @@ class App {
         var whitelist = ["https://www.faktoryel.com"];
         var corsOptions = {
             origin: function (origin, callback) {
-                if (whitelist.indexOf(origin) !== -1 || !origin) {
+                if (whitelist.indexOf(origin) !== -1) {
                     callback(null, true);
                 }
                 else {
